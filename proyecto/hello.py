07748814c67b4f4fe8d1223c79d635c279dc59a8
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2020-11-18 00:33:15 -0600

    Markov chains algorithm
    Signed-off-by: Victor Javier Aguayo Mendoza <a01229260@itesm.mx>

--- proyecto/hello.py
+++ proyecto/hello.py
@@ -1,20 +1,35 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
-
+import project as backend
 app = Flask(__name__)
+
+#Array to store globally the teams selected
+teams=[]
+#Array to store the data to display on the results page
+results=[]
+#Data to use
+data=[]
+#Path to read and write data, working directory.
+WORKING_PATH  = 'D:\\7mo semestre\\Metodos cuantitativos\\MetodosCuantitativos\\proyecto\\'
+#Filename of the original data file.
+FILE_NAME = 'data.json'
+#Complete path of the original data file
+PATH = FILE_NAME
 
 
 @app.route('/')
 def index():
    return render_template(
       "llaves.html",
-      data =[{'team': 'Top esports'}, {'team': 'G2 Esports'}, {'team': 'Damwon'}, {'team': 'Suning'}, {'team': 'DRX'}, 
+      data =[{'team': 'Top Esports'}, {'team': 'G2 Esports'}, {'team': 'Damwon'}, {'team': 'Suning Gaming'}, {'team': 'DragonX'}, 
       {'team': 'JD Gaming'}, {'team': 'Fnatic'}, {'team': 'Gen G'}, {'team': 'Team Liquid'}, {'team': 'PSG Talon'},
-      {'team': 'LGD Gaming'}, {'team': 'FlyQuest'}, {'team': 'Machi Esport'}, {'team': 'Rogue'}, {'team': 'TSM'},
-      {'team': 'Unicorns Of Love'}, ])
+      {'team': 'LGD Gaming'}, {'team': 'FlyQuest'}, {'team': 'Machi Esports'}, {'team': 'Rogue'}, {'team': 'TSM'},
+      {'team': 'Unicorns of Love'}, ])
 
 @app.route('/resultado', methods=['GET', 'POST'])
 def resultado():
+   global teams
+   global data
    team1 = request.form.get('team1')
    team2 = request.form.get('team2')
    team3 = request.form.get('team3')
@@ -23,9 +38,17 @@ def resultado():
    team6 = request.form.get('team6')
    team7 = request.form.get('team7')
    team8 = request.form.get('team8')
-   arrayTeam = [team1,team2,team3,team4,team5,team6,team7,team8]
-   return render_template('resultado.html',  team1 = team1, team2 = team2, team3 = team3, team4 = team4, team5 = team5, 
-   team6 = team6, team7 = team7, team8 = team8 ) 
+   teams = [team1,team2,team3,team4,team5,team6,team7,team8]
+
+   data = backend.loadData(PATH)
+   backend.settingBuffs(data)
+   data = backend.loadData(WORKING_PATH+"buffed_data.json")
+   tournament=backend.getWinner(teams,data)
+
+   #return render_template('resultado.html',  team1 = team1, team2 = team2, team3 = team3, team4 = team4, team5 = team5, 
+   #team6 = team6, team7 = team7, team8 = team8 ) 
+   return render_template('resultado.html', tournament=tournament)
 
 if __name__=='__main__':
     app.run(debug=True)
+
---
+++ proyecto/project.py
@@ -0,0 +1,201 @@
+import json
+import random as rnd
+
+#Path to read and write data, working directory.
+WORKING_PATH  = 'D:\\7mo semestre\\Metodos cuantitativos\\MetodosCuantitativos\\proyecto\\'
+#Filename of the original data file.
+FILE_NAME = 'data.json'
+#Complete path of the original data file
+PATH = FILE_NAME
+
+#Set the data with buffs and debuffs according
+#to the region of the team
+def settingBuffs(data):
+    buffs={"LPL":1,"LEC":0.98,"LCK":0.95,"LCS":0.90,"PCS":0.85,"LCL":0.55}
+    buffed_data=data
+    for team in data:
+        mult = buffs[data[team]["region"]]
+        for stat in data[team]:
+            if stat=="region" or stat=="goldpm":
+                continue
+            elif stat=="members":
+                for member in data[team][stat]:
+                    new_stat = round(data[team][stat][member]["killparticipation"]*mult,2)
+                    buffed_data[team][stat][member]["killparticipation"]=new_stat
+            else:
+                new_stat = round(data[team][stat]*mult,2)
+                buffed_data[team][stat]=new_stat
+
+    #Write the new buffed data on a json file
+    with open(WORKING_PATH+'buffed_data.json','w') as outfile:
+        json_object= json.dumps(buffed_data, indent=4)
+        outfile.write(json_object)
+        print("Buffed data file created succesfully")
+    
+#Returns the dictionary load from a json file.
+def loadData(path):
+    with open(path,'r') as f:
+	    data = json.load(f)
+    return data
+
+#Receives 2 porcentages and returns which is the winner.
+#The porcentages can sum over 100%, this function translate
+#them to a 100% basis. This method is a Markov chain of only 2
+#posible outcomes. Returns 1 or 2, depending of the winner
+def markov(teamA_ptge, teamB_ptge):
+    sum_ptges=teamA_ptge+teamB_ptge
+    teamA_ptge/=sum_ptges
+    teamB_ptge/=sum_ptges
+    w=[teamA_ptge,teamB_ptge]
+    return rnd.choices([1,2],weights=tuple(w), k=1)[0]
+
+#Simulates a match between 2 teams, considering all the statistics
+#and solving each of them with markov chains. Receives 2 team
+#names and the data. Returns a list with the name of the winner and the
+#porcentages in order (teamA and then teamB).
+def playMatch(teamA, teamB,data):
+
+    #Here we have the values of each stat in game according
+    #to our own model of probabilities. 
+    value_ingame={
+        "winrate": 30,
+        "firstblood": 15,
+        "firsttower": 5,
+        "1dragon": 5,
+        "2dragon": 10,
+        "3dragon": 20,
+        "4dragon": 30,
+        "elderdragon": 30,
+        "heraldpg": 10,
+        "nashorpg": 25,
+        "top": 5,
+        "jg": 5,
+        "mid": 5,
+        "adc": 5,
+        "supp": 5
+    }
+
+    #Here we will sum the stats that each team won
+    teamA_sum=0
+    teamB_sum=0
+
+    for stat in data[teamA]:
+        if stat=="region" or stat=="goldpm" or stat=="towerratio":
+                #Irrelevant information in the database
+                continue
+        elif stat=="members":
+            #Members stats
+            for member in data[teamA][stat]:
+                for member2 in data[teamB][stat]:
+                    if data[teamA][stat][member]["position"]==data[teamB][stat][member2]["position"]:
+                        if markov(data[teamA][stat][member]["killparticipation"],data[teamB][stat][member2]["killparticipation"])==1:
+                            teamA_sum+=value_ingame[data[teamA][stat][member]["position"]]
+                        else:
+                            teamB_sum+=value_ingame[data[teamB][stat][member2]["position"]]
+        elif stat=="dragonpg":
+            teamA_dragons=0
+            teamB_dragons=0
+            #Normal dragons
+            while teamA_dragons<4 and teamB_dragons<4:
+                if markov(data[teamA][stat],data[teamB][stat])==1:
+                    teamA_dragons+=1
+                    teamA_sum+=value_ingame[str(teamA_dragons)+"dragon"]
+                else:
+                    teamB_dragons+=1
+                    teamB_sum+=value_ingame[str(teamB_dragons)+"dragon"]
+            
+            #Elder dragon
+            if markov(data[teamA][stat],data[teamB][stat])==1:
+                teamA_sum+=value_ingame["elderdragon"]
+            else:
+                teamB_sum+=value_ingame["elderdragon"]
+        else:
+            if markov(data[teamA][stat],data[teamB][stat])==1:
+                teamA_sum+=value_ingame[stat]
+            else:
+                teamB_sum+=value_ingame[stat]
+        #print(teamA_sum)
+        #print(teamB_sum)
+
+    #Markov chain with final porcentages. Determines the winner
+    sum_results=teamA_sum+teamB_sum
+    teamA_ptge=teamA_sum/sum_results
+    teamB_ptge=teamB_sum/sum_results
+    if markov(teamA_ptge,teamB_ptge)==1:
+        winner=teamA
+    else:
+        winner=teamB
+
+    return [winner,teamA_ptge, teamB_ptge]
+
+#Receives 8 teams participating on the bracket and the
+# data. Returns a winner depending on the Markov chains
+#representing each match
+def getWinner(teams,data):
+    result={}
+    winners=[]
+    labels=["Quarterfinals","Semifinals","Final"]
+    cont_label=0
+    match_cont=0
+    while len(teams)>1:
+        print(labels[cont_label]+"\n--------------------\n")
+        cont_label+=1
+        for index in range(0,len(teams),2):
+            match_cont+=1
+            teamA_wins=0
+            teamB_wins=0
+            teamA_ptge=0
+            teamB_ptge=0
+
+            #Store all the information on a dictionary to send it to the frontend
+            result["serie"+str(match_cont)]={"teamA":teams[index]}
+            result["serie"+str(match_cont)]["teamB"]=teams[index+1]
+
+            #Bo5
+            while teamA_wins<3 and teamB_wins<3:
+                print("Playing "+teams[index]+" vs "+teams[index+1])
+                match_results=playMatch(teams[index],teams[index+1],data)
+                print("Winner: "+match_results[0]+"\nPercentages: ")
+                print(teams[index]+": "+str(round(match_results[1]*100,2))+"%")
+                print(teams[index+1]+": "+str(round(match_results[2]*100,2))+"%")
+                teamA_ptge+=round(match_results[1]*100,2)
+                teamB_ptge+=round(match_results[2]*100,2)
+                print("------------------------------\n")
+                if match_results[0]==teams[index]:
+                    teamA_wins+=1
+                elif match_results[0]==teams[index+1]:
+                    teamB_wins+=1
+                print(teamB_wins+teamA_wins)
+                if teamA_wins+teamB_wins==1:
+                    result["serie"+str(match_cont)]["matches"]={"match"+str(teamA_wins+teamB_wins):{"winner":match_results[0]}}
+                else:
+                    result["serie"+str(match_cont)]["matches"]["match"+str(teamA_wins+teamB_wins)]={"winner":match_results[0]}
+                result["serie"+str(match_cont)]["matches"]["match"+str(teamA_wins+teamB_wins)]["teamA_ptge"]=match_results[1]
+                result["serie"+str(match_cont)]["matches"]["match"+str(teamA_wins+teamB_wins)]["teamB_ptge"]=match_results[2]
+            if teamA_wins>=3:
+                winner=teams[index]
+            elif teamB_wins>=3:
+                winner=teams[index+1]
+            winners.append(winner)
+            teamA_ptge/=(teamA_wins+teamB_wins)
+            teamB_ptge/=(teamA_wins+teamB_wins)
+
+            result["serie"+str(match_cont)]["winner"]=winner
+            result["serie"+str(match_cont)]["teamA_ptge"]=teamA_ptge
+            result["serie"+str(match_cont)]["teamB_ptge"]=teamB_ptge
+            result["serie"+str(match_cont)]["teamA_score"]=teamA_wins
+            result["serie"+str(match_cont)]["teamB_score"]=teamB_wins
+        teams=winners
+        winners=[]
+    print("CAMPEON: "+teams[0])
+    #print(result)
+    return result
+    
+
+
+data = loadData(PATH)
+settingBuffs(data)
+data = loadData(WORKING_PATH+"buffed_data.json")
+#Teams that will be received from the user
+teams=["Damwon","DragonX","Gen G","G2 Esports","Top Esports","Fnatic","Suning Gaming","JD Gaming"]
+getWinner(teams,data)
